aea07182d23b1a52337cfd833a4102fa1eea1406
CTBAV\alexandru.razvant <alexandru.razvant@ctbav.ro>
2018-08-27 16:28:31 +0300

    Recipe webpage is ready. [BUG] undefined for form-controls

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -9,14 +9,23 @@ import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@ang
 export class RecipesComponent implements OnInit {
 
   public selectedValue;
-  @Input() options : Array<String> = ['Cook','Mix','Cool','Package']
-  recipesArray = new FormArray([]);
+  recipeActionsArray = ['Mix', 'Cook'];
+  recipeMaterialsArray = ['Potatoes', 'Sunflower Oil', 'Salt', 'Flavours', 'Paprika', 'BBQ', 'Pepper', 'Potatoes', 'Flour', 'Cheese', 'Starch'];
+  recipeMeasurementUnitsArray = ['kg', 'g', 'mg'];
+  recipeActionsTimeUnitsArray = ['min', 'hours', 'days'];
+  ingredientsArray = new FormArray([]);
+  actionsArray = new FormArray([]);
   recipesFormGroup: FormGroup;
   constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
     this.recipesFormGroup = new FormGroup({
-      'recipes': this.recipesArray
+      'ingredients': this.ingredientsArray,
+      'actions': new FormGroup({
+        'ActionName': new FormControl(null, Validators.required),
+        'ActionTime': new FormControl(null, Validators.required),
+        'MeasureUnit': new FormControl(null, Validators.required)
+      })
     })
   }
   onAddRecipe() {
@@ -24,12 +33,23 @@ export class RecipesComponent implements OnInit {
       'FeedStock': new FormControl(null, Validators.required),
       'Quantity': new FormControl(null, Validators.required),
       'Measurement': new FormControl(null, Validators.required),
-      'Action': new FormControl(null, Validators.required),
     });
-    (<FormArray>this.recipesArray).push(recipeCreationGroup);
+    (<FormArray>this.recipesFormGroup.get('ingredients')).push(recipeCreationGroup);
   }
 
   checkIfCanSelectTime(this) {
-   
+
+  }
+
+  deleteRowAt(index: number) {
+    const control = <FormArray>this.recipesFormGroup.controls['recipes'];
+    if (confirm("Are you sure?")) {
+      // remove the chosen row
+      control.removeAt(index);
+    }
+  }
+
+  finishRecipe() {
+    console.log(this.recipesFormGroup);
   }
 }
